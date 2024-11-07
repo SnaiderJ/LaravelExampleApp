c6d50907556dd8287553215fbd42e24aa44ed5e9
in2dm3-v <in2dm3-v@elorrieta.int>
2024-11-07 19:47:21 +0100

    Se ha hecho uso de la plantilla general que se ha generado en las vistas index y show

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,16 @@
-<ul>
-  {{--esto es un comentario: recorremos el listado de posts--}}
-  @foreach ($posts as $post)
-    {{-- visualizamos los atributos del objeto --}}
-    <li>
-      <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>.
-      Escrito el {{$post->created_at}}
-    </li>
-  @endforeach
-</ul>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <ul>
+    {{--esto es un comentario: recorremos el listado de posts--}}
+    @foreach ($posts as $post)
+      {{-- visualizamos los atributos del objeto --}}
+      <li>
+        <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>.
+        Escrito el {{$post->created_at}}
+      </li>
+    @endforeach
+  </ul>
+</div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,9 @@
-{{$post->titulo}}
-{{$post->texto}}
-{{$post->publicado}}
-creado el {{$post->created_at}}
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <h1>{{$post->titulo}}</h1>
+  <p>Creado el {{$post->created_at}}</p>
+  <p>{{$post->texto}}</p>
+</div>
+@endsection
